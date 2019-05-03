22c194c829127dc9556b90209a3748cd9ea7fcb2
rahulkumar3214 <44266770+rahulkumar3214@users.noreply.github.com>
2019-05-03 13:44:41 +0530

    Add files via upload

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { AuthenticationService } from '../orderservice/authentication.service';
+//import { AlertService } from '../orderservice/alert.service';
+import { CustomerService } from '../orderservice/customer.service';
+import {CookieService} from 'angular2-cookie/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+newPage: boolean;
+loginForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
+  authenticationService: any;
+  alertService: any;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private _cookieService:CookieService
+        //private authenticationService: AuthenticationService,
+      // private alertService: AlertService) 
+     ) {
+      if(_cookieService.get('remember')) {
+        this.Formdata.username=this._cookieService.get('username');
+        this.Formdata.password=this._cookieService.get('password');
+        this.Formdata.rememberme=this._cookieService.get('remember');
+     }
+     }
+
+        ngOnInit() {
+          this.loginForm = this.formBuilder.group({
+              username: ['', Validators.required],
+              password: ['', Validators.required]
+          });
+        
+          // reset login status
+        //this.authenticationService.logout();
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/customer';
+      }
+
+    get f() { return this.loginForm.controls; }
+    onSubmit() {
+      this.submitted = true;
+
+      if (this.loginForm.invalid) {
+          return;
+       }
+      //this.loading = true;
+      this.router.navigate([this.returnUrl]);
+      // this.authenticationService.login(this.f.username.value, this.f.password.value)
+      //     .pipe(first())
+      //     .subscribe(
+      //         data => {
+      //             this.router.navigate([this.returnUrl]);
+      //         },
+      //         error => {
+      //             this.alertService.error(error);
+      //             this.loading = false;
+      //         });
+  }
+
+  public Formdata:any = {};
+
+        
+        submitData() {
+       this._cookieService.put('username',this.Formdata.username);
+       this._cookieService.put('password',this.Formdata.password);
+       this._cookieService.put('remember',this.Formdata.rememberme);
+    }
+  
+
+
+
+
+}
---
+++ src/app/orderservice/customer.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+//import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+  customer: any;
+  // deleteCustomer(key: any) {
+  //   throw new Error("Method not implemented.");
+  // }
+
+  // constructor(private firebase: AngularFireDatabase) { }
+  // customerList: AngularFireList<any>;
+  form = new FormGroup({
+    //$key: new FormControl(null),
+    orderNumber: new FormControl('', Validators.required),
+    orderDueDate: new FormControl(),
+    buyerName: new FormControl(''),
+    address: new FormControl(''),
+    phone: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    totalOrder: new FormControl('')
+  });
+ customerArray = [
+    {"orderNumber":"1", "orderDueDate":"1992-02-15", "buyerName":"rahul", "address":"gachibowli", "phone":"9642634019", "totalOrder":"555556" },
+    {"orderNumber":"2", "orderDueDate":"1992-02-16", "buyerName":"Kumar", "address":"indira", "phone":"3223262626", "totalOrder":"5000" },
+    {"orderNumber":"3", "orderDueDate":"1992-02-17", "buyerName":"Navratan", "address":"delhi", "phone":"3215666466", "totalOrder":"500" },
+    {"orderNumber":"4", "orderDueDate":"1992-02-18", "buyerName":"Technologies", "address":"kolkata", "phone":"8876256555", "totalOrder":"2000" },
+    {"orderNumber":"5", "orderDueDate":"1992-02-19", "buyerName":"E", "address":"hyderbad", "phone":"9654552233", "totalOrder":"5455200" }
+  ];
+  // getCustomers() {
+  //   this.customerList = this.firebase.list('customers');
+  //   return this.customerList.snapshotChanges();
+  // }
+  
+  insertCustomer(customer) {
+    var addData :any = customer.orderNumber;
+    var indexCustomer = this.customerArray.findIndex(
+      a => a.orderNumber=== addData);
+      var obj=this.customerArray[indexCustomer];
+      
+     if(indexCustomer == -1) {
+     this.customerArray.push({
+      orderNumber: customer.orderNumber,
+      orderDueDate: customer.orderDueDate,
+      buyerName: customer.buyerName,
+      address: customer.address,
+      phone: customer.phone,
+       totalOrder: customer.totalOrder
+     });
+          }     
+     //this.form.reset();
+      if(addData == obj.orderNumber){
+        this.customerArray.splice(indexCustomer,1,this.form.value);
+      }
+      //this.form.reset();
+    }
+    
+     populateForm(customer) {
+      //this.form.setValue(customer);
+       this.form.setValue(customer);
+       
+      
+      //  var addData= this.customer.orderNumber;
+      //      var indexCustomer = this.customerArray.findIndex(
+      //      customer => customer.orderNumber==="this.customerArray.orderNumber");
+      //      if(addData == this.customerArray[indexCustomer].orderNumber){
+      //    this.customerArray=this.customerArray.splice(indexCustomer,1,this.form.value);
+      //  }
+      }
+    // updateCustomer(customer){
+    //   this.customerArray.({
+    //   orderNumber: customer.orderNumber,
+    //   orderDueDate: customer.orderDueDate,
+    //   buyerName: customer.buyerName,
+    //   address: customer.address,
+    //   phone: customer.phone,
+    //   totalOrder: customer.totalOrder
+    //   });
+    //}
+  
+    // deleteCustomer($key: string) {
+    //   this.customerList.remove($key);
+    }
+  
+
+
+
